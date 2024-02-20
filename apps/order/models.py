5dd9cab76f493dc08bbc8152612a7f61899e7cba
ibragimovxusan <lazy.devsloper@gmail.com>
2024-02-20 20:25:45 +0500

    Add WishList, ShopCart, Order, and OrderItem models

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,4 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.base.models import BaseModel
+from apps.product.models import Product
+
+
+class WishList(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_wishlists')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='wishlists')
+
+    def __str__(self):
+        return f"{self.user} - {self.product}"
+    
+class ShopCart(BaseModel):
+    is_complated = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_carts')
+
+    def __str__(self):
+        return f"{self.user} - {self.is_complated}"
 
 
 
+class Order(BaseModel):
+    STATUS = (
+        ('PENDING', 'PENDING'),
+        ('COMPLETED', 'COMPLETED'),
+        ('CANCELED', 'CANCELED'),
+    )
+    status = models.CharField(max_length=50, choices=STATUS, default='PENDING')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_orders')
+    phone_numeber = models.CharField(max_length=50)
+    
+
+
+class OrderItem(BaseModel):
+    cart = models.ForeignKey(ShopCart, on_delete=models.CASCADE, related_name='cart_items')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items', null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.cart} - {self.product} - {self.quantity}"
