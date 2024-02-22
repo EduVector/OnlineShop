9367304134cf45e069ee6828f3dfa4fd704e2d3b
saintdeveloper <jons_artym04@mail.ru>
2024-02-22 17:23:32 +0300

    main

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
+from .models import WishList,ShopCart,Order,OrderItem
 
 # Register your models here.
+
+@admin.register(WishList)
+class WishList(DraggableMPTTAdmin):
+    mptt_indent_field = "name"
+
+@admin.register(ShopCart)
+class AdminShopCart(DraggableMPTTAdmin):
+    mptt_indent_field = "name"
+
+@admin.register(Order)
+class AdminOrder(DraggableMPTTAdmin):
+    mptt_indent_field = "name"
+
+@admin.register(OrderItem)
+class AdminOrderItem(DraggableMPTTAdmin):
+    mptt_indent_field = "name"
