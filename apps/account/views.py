8a30bd304fc92e0c63e3fb1d01822237de2740f3
ibragimovxusan <lazy.devsloper@gmail.com>
2024-02-24 20:12:16 +0500

    Add index view to product app and register view to account app

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name", None)
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+
+        user = User.objects.filter(username=username).first()
+        if user:
+            messages.error(request, "User already exists!")
+            return redirect('register')
+        else:
+            if password != password2:
+                messages.error(request, "Password did not match!")
+                return redirect("register")
+            
+            instance = User.objects.create_user(
+                                    username=username,
+                                    first_name=first_name, 
+                                    last_name=last_name,
+                                    password=password
+                                )
+            
+            user = authenticate(username=username, password=password)    
+            login(request, user)
+            messages.success(request, "Successfully created user!")
+            return redirect('/')
+        
+    return render(request, 'auth/register.html')
+
+
+
+
+
+
+            
