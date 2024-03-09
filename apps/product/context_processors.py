4f29ca15e37b6da68713878eceb5cfa24df071e1
husanibragimov <ibragmovhusan283@gmail.com>
2024-03-09 20:23:22 +0500

    Register GetInTouch and Branch models in admin.py, remove unnecessary status options in models.py, add top_sales and top_rates in views.py, add email subscription functionality in context_processors.py

--- apps/product/context_processors.py
+++ apps/product/context_processors.py
@@ -1,10 +1,14 @@
+from django.db.models import Q
+from django.contrib import messages
+from apps.contact.models import GetInTouch
 from .models import Category, Brand, Product
 from apps.order.models import ShopCart, WishList, Order
-from django.db.models import Q
+
 
 
 def get_processors(request):
     q = request.GET.get('q')
+    email = request.GET.get('getouch')
 
     categories = Category.objects.all()
     parent_cats = Category.objects.filter(level=0)
@@ -15,6 +19,10 @@ def get_processors(request):
         products = products.filter(
             Q(name__icontains=q) | Q(categories__name__icontains=q)
         )
+    
+    if email:
+        GetInTouch.objects.create(email=email, full_name="From Subscribe")
+        messages.success(request, "Successfully subscribed")
 
 
     context = {
--- apps/product/views.py
+++ apps/product/views.py
@@ -17,13 +17,20 @@ class HomePage(View):
         new_products = Product.objects.filter(is_active=True).order_by('-created_at')
         parent_cats = Category.objects.filter(level=0).order_by('?')
 
+        top_sales = Product.objects.filter(status='SALE').order_by('?')
+        top_rates = Product.objects.all().order_by('-product_rates__rate')
+
 
         context = {
             'products': products[:16],
             'new_products': new_products[:16],
             'parent_cats': parent_cats[:5],
             'popular_products': popular_products[:16],
-            'new_arrivals': products[10:20],
+            'new_arrivals': products[:20],
+            'top_views': popular_products[:3],
+            'top_sales': top_sales[:3],
+            'top_rates': top_rates[:3]
+
         }
         return render(request, 'product/index.html', context)
 
